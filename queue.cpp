b4426601dc6102ba25b0b201dac0a042042f14d7
Han <lovingamtist@gmail.com>
2025-06-13 22:00:35 +0900

    queue.cpp_enqueue()를 힙에 적용하여 변경

--- queue.cpp
+++ queue.cpp
@@ -2,6 +2,8 @@
 
 #include "queue.h"
 
+#define MAX_SIZE 100000
+
 Queue* init(void) {
     Queue* q = new Queue();
     q->size = 0;
@@ -64,99 +66,81 @@ Node* nclone(Node* node) {
 }
 
 
-
-
 Reply enqueue(Queue* queue, Item item) {
     std::lock_guard<std::mutex> lock(queue->mtx);
-    Reply reply = {false, item};
+    Reply reply = { false, {0, nullptr, 0} };
 
-    Node* prev = nullptr;
-    Node* cur = queue->head;
-
-    while (cur && cur->item.key < item.key) {
-        prev = cur;
-        cur = cur->next;
-    }
+    if (queue->size >= MAX_SIZE) return reply;
 
     
-    if (cur && cur->item.key == item.key) {
-        delete[] (char*)cur->item.value;
-        cur->item.value = new char[item.value_size];
-        std::memcpy(cur->item.value, item.value, item.value_size);
-        cur->item.value_size = item.value_size;
 
-        reply.success = true;
-        reply.item = cur->item;
-        return reply;
-    }
+    //깊은복사구현부분
+    Item& new_item = queue->heap[queue->size];
+    new_item.key = item.key;
+    new_item.value_size = item.value_size;
+    new_item.value = new char[item.value_size];
+    //
+    memcpy(new_item.value, item.value, item.value_size);
 
-    
-    Node* new_node = nalloc(item);
-    if (!prev) {
-        new_node->next = queue->head;
-        queue->head = new_node;
-    } else {
-        new_node->next = cur;
-        prev->next = new_node;
-    }
+    heapify_up(queue, queue->size);
 
     reply.success = true;
-    reply.item = new_node->item;
+    reply.item = new_item;
+    queue->size++;
     return reply;
 }
 
 
+// Reply dequeue(Queue* queue) {
+//     std::lock_guard<std::mutex> lock(queue->mtx);
+//     Reply reply = {false, {}};
 
-Reply dequeue(Queue* queue) {
-    std::lock_guard<std::mutex> lock(queue->mtx);
-    Reply reply = {false, {}};
+//     if (!queue->head) return reply;
 
-    if (!queue->head) return reply;
+//     Node* node = queue->head;
+//     queue->head = node->next;
 
-    Node* node = queue->head;
-    queue->head = node->next;
+//     reply.success = true;
+//     reply.item.key = node->item.key;
+//     reply.item.value_size = node->item.value_size;
+//     if (node->item.value_size > 0 && node->item.value != nullptr) {
+//         reply.item.value = new char[node->item.value_size];
+//         std::memcpy(reply.item.value, node->item.value, node->item.value_size);
+//     } else {
+//         reply.item.value = nullptr;
+//     }
 
-    reply.success = true;
-    reply.item.key = node->item.key;
-    reply.item.value_size = node->item.value_size;
-    if (node->item.value_size > 0 && node->item.value != nullptr) {
-        reply.item.value = new char[node->item.value_size];
-        std::memcpy(reply.item.value, node->item.value, node->item.value_size);
-    } else {
-        reply.item.value = nullptr;
-    }
+//     nfree(node);
+//     return reply;
+// }
 
-    nfree(node);
-    return reply;
-}
+// Queue* range(Queue* queue, Key start, Key end) {
+//     Queue* result = init();
+//     std::lock_guard<std::mutex> lock(queue->mtx);
 
-Queue* range(Queue* queue, Key start, Key end) {
-    Queue* result = init();
-    std::lock_guard<std::mutex> lock(queue->mtx);
+//     Node* cur = queue->head;
+//     Node** tail = &result->head;
 
-    Node* cur = queue->head;
-    Node** tail = &result->head;
+//     while (cur) {
+//         Key k = cur->item.key;
+//         if (k > end) break;
 
-    while (cur) {
-        Key k = cur->item.key;
-        if (k > end) break;
+//         if (k >= start) {
+//             Node* copy = new Node();
+//             copy->item.key = cur->item.key;
+//             copy->item.value_size = cur->item.value_size;
+//             if (cur->item.value_size > 0 && cur->item.value != nullptr) {
+//                 copy->item.value = new char[cur->item.value_size];
+//                 std::memcpy(copy->item.value, cur->item.value, cur->item.value_size);
+//             } else {
+//                 copy->item.value = nullptr;
+//             }
+//             copy->next = nullptr;
+//             *tail = copy;
+//             tail = &copy->next;
+//         }
+//         cur = cur->next;
+//     }
 
-        if (k >= start) {
-            Node* copy = new Node();
-            copy->item.key = cur->item.key;
-            copy->item.value_size = cur->item.value_size;
-            if (cur->item.value_size > 0 && cur->item.value != nullptr) {
-                copy->item.value = new char[cur->item.value_size];
-                std::memcpy(copy->item.value, cur->item.value, cur->item.value_size);
-            } else {
-                copy->item.value = nullptr;
-            }
-            copy->next = nullptr;
-            *tail = copy;
-            tail = &copy->next;
-        }
-        cur = cur->next;
-    }
-
-    return result;
-}
+//     return result;
+// }
